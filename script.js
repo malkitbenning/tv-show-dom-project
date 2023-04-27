7a36bc0062a9a8c8b604e763acbdec09cdf00cba
malkitbenning <malkitbenning@gmail.com>
2023-04-27 15:03:50 +0100

    title and image being displayed
    basic start

--- script.js
+++ script.js
@@ -7,6 +7,54 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  console.log(episodeList[0].name);
+  let episodesContainer = document.createElement("div");
+  episodesContainer.classList.add("episodes-container");
+  rootElem.appendChild(episodesContainer);
+  for (episode of episodeList) {
+    let episodeHolder = document.createElement("div");
+    episodeHolder.classList.add("episode-holder");
+    episodesContainer.appendChild(episodeHolder);
+
+    // episode title area
+
+    let episodeTitleArea = document.createElement("div");
+    episodeTitleArea.classList.add("episode-title-area");
+    episodeHolder.appendChild(episodeTitleArea);
+
+    // episode name
+
+    let episodeName = document.createElement("span");
+    episodeName.classList.add("episode-name");
+    episodeName.textContent = episode.name;
+    episodeTitleArea.appendChild(episodeName);
+
+    // episode code
+
+    let seasonPadded = episode.season.toString().padStart(2, "0");
+    let episodeNumberPadded = episode.number.toString().padStart(2, "0");
+    let episodeCodeValue = " - S" + seasonPadded + "E" + episodeNumberPadded;
+    let episodeCode = document.createElement("span");
+    episodeCode.classList.add("episode-code");
+    episodeCode.textContent = episodeCodeValue;
+    episodeTitleArea.appendChild(episodeCode);
+
+    // image title area
+
+    let imageTitleArea = document.createElement("div");
+    imageTitleArea.classList.add("image-title-area");
+    episodeHolder.appendChild(imageTitleArea);
+
+    // image - size medium
+
+    let episodeImageMediumURL = episode.image.medium;
+    let episodeImageMedium = document.createElement("img");
+    episodeImageMedium.classList.add("episode-image-medium");
+    episodeImageMedium.src = episodeImageMediumURL;
+    imageTitleArea.appendChild(episodeImageMedium);
+  }
+  console.log(episodeList);
 }
 
 window.onload = setup;
+
